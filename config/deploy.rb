d82deabdb376f2bfb66b748b0c078be4bab35ecb
Yu Hoshino <yuhoshino@dev.local>
2015-07-27 00:39:54 +0900

    restart unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,3 +7,11 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.1.6'
 set :rbenv_map_bins, %w(rake gem bundle ruby rails)
 set :rbenv_roles, :all
+set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/assets)
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
