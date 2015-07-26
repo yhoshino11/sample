9eafda410b53e719a42873a783ab471cf61ec954
Yu Hoshino <yuhoshino@dev.local>
2015-07-27 00:25:46 +0900

    capistrano worked!

--- Capfile
+++ Capfile
@@ -2,5 +2,8 @@ require 'capistrano/setup'
 require 'capistrano/deploy'
 require 'capistrano/rails'
 require 'capistrano/rbenv'
-
+set :rbenv_custom_path, '/home/deploy/.rbenv'
+set :rbenv_ruby, '2.1.6'
+set :deploy_to, '/home/deploy/sample'
+set :scm, :git
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,39 +1,10 @@
 set :stage, :production
 set :rails_env, 'production'
-server '192.168.179.9', user: 'root', roles: %w(web app db)
-set :ssh_options, keys: [File.expand_path('~/.ssh/id_rsa)')]
-
-# server 'example.com', user: 'deploy', roles: %w{app db web},
-# server 'example.com', user: 'deploy', roles: %w{app web}
-# server 'db.example.com', user: 'deploy', roles: %w{db}
-
-# role :app, %w{deploy@example.com}
-# role :web, %w{user1@primary.com user2@additional.com}
-# role :db,  %w{deploy@example.com}
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+key_path = '~/.ssh/capistrano'
+server '192.168.179.9', roles: %w(web app db),
+                        user: 'deploy',
+                        ssh_options: {
+                          user: 'deploy',
+                          keys: key_path,
+                          auth_methods: %w(publickey)
+                        }
